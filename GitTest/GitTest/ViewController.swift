35bd9212d6cbdbeeb71ca21a699f56d8a51298be
jinShine <seungjin429@naver.com>
2019-01-13 13:11:06 +0900

    Github 프로젝트 생성
    깃과 깃허브 공부용 프로젝트
    init프로젝트 생성

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by 승진김 on 13/01/2019.
+//  Copyright © 2019 seungjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
