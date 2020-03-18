43a61f997f229a1b9c7d99c62fa1296007ff943c
jinnify <seungjin429@gmail.com>
2020-03-18 17:55:11 +0900

    js & jqeury

---
+++ 3Day/2.js_ex1.js
@@ -0,0 +1,454 @@
+/*
+    1. 
+    1,2,3,4 .. 높여가며 숫자를 보여주는 onclick 함수 만들어보기
+*/
+
+{/* <script>
+    let num = 0
+    let maxNum = 5
+    
+    function hello() {
+        num += 1
+    
+        if (num <= maxNum) {
+            alert(num + "번 누르셨습니다." + " (" + (maxNum - num) + " 남음)")
+        } else {
+            alert("최대 5번까지 입니다.")
+        }
+    }
+</script> */}
+
+
+/*
+    2.
+    배열에서 원소의 갯수를 세기 - ("딸기는 몇개일까?")
+*/
+
+
+let a = ['사과','감','감','배','포도','포도','딸기','포도','감','수박','딸기']
+let count = 0
+
+a.forEach(element => {
+    if (element == '딸기') {
+        count += 1
+    }
+})
+
+console.log(count)
+
+
+// 함수로 만들어보기
+
+function count_fruit(names) {
+    let count = 0
+
+    names.forEach(name => {
+        if (name == '딸기') {
+            count += 1
+        }
+    })
+
+    return count
+}
+
+count_fruit(a)
+
+/*
+    3.
+    0부터 n-1까지 더하는 함수를 만들고 싶다면?
+*/
+
+function get_sum(n) {
+    let result = 0
+    for (let i = 0; i < n; i++) {
+      result = result + i;
+          // 반복문을 돌면서, result에다 i를 더해줍니다.
+          // result += i; 라고 줄여 쓸 수 있습니다.
+    }
+      return result
+  }
+  
+console.log(get_sum(10)) // 45를 출력
+
+/*
+    4.
+    미세먼지(IDEX_MVL)의 값이 50 미만인 구 이름(MSRSTE_NM)과 값을 출력하기
+*/
+
+let mise = [
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "도심권",
+        MSRSTE_NM: "중구",
+        PM10: 22,
+        PM25: 14,
+        O3: 0.018,
+        NO2: 0.015,
+        CO: 0.4,
+        SO2: 0.002,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 31,
+        ARPLT_MAIN: "O3"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "도심권",
+        MSRSTE_NM: "종로구",
+        PM10: 24,
+        PM25: 18,
+        O3: 0.013,
+        NO2: 0.016,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 39,
+        ARPLT_MAIN: "PM25"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "도심권",
+        MSRSTE_NM: "용산구",
+        PM10: 0,
+        PM25: 15,
+        O3: 0.012,
+        NO2: 0.027,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "점검중",
+        IDEX_MVL: -99,
+        ARPLT_MAIN: "점검중"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서북권",
+        MSRSTE_NM: "은평구",
+        PM10: 36,
+        PM25: 14,
+        O3: 0.019,
+        NO2: 0.018,
+        CO: 0.5,
+        SO2: 0.005,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 42,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서북권",
+        MSRSTE_NM: "서대문구",
+        PM10: 28,
+        PM25: 9,
+        O3: 0.018,
+        NO2: 0.015,
+        CO: 0.6,
+        SO2: 0.004,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 37,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서북권",
+        MSRSTE_NM: "마포구",
+        PM10: 26,
+        PM25: 8,
+        O3: 0.012,
+        NO2: 0.021,
+        CO: 0.5,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 36,
+        ARPLT_MAIN: "NO2"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "광진구",
+        PM10: 17,
+        PM25: 9,
+        O3: 0.018,
+        NO2: 0.016,
+        CO: 0.6,
+        SO2: 0.001,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 31,
+        ARPLT_MAIN: "O3"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "성동구",
+        PM10: 21,
+        PM25: 12,
+        O3: 0.018,
+        NO2: 0.017,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 33,
+        ARPLT_MAIN: "PM25"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "중랑구",
+        PM10: 27,
+        PM25: 10,
+        O3: 0.015,
+        NO2: 0.019,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 34,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "동대문구",
+        PM10: 26,
+        PM25: 9,
+        O3: 0.016,
+        NO2: 0.017,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 34,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "성북구",
+        PM10: 27,
+        PM25: 8,
+        O3: 0.022,
+        NO2: 0.014,
+        CO: 0.5,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 37,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "도봉구",
+        PM10: 25,
+        PM25: 12,
+        O3: 0.024,
+        NO2: 0.011,
+        CO: 0.3,
+        SO2: 0.002,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 41,
+        ARPLT_MAIN: "O3"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "강북구",
+        PM10: 30,
+        PM25: 15,
+        O3: 0.022,
+        NO2: 0.02,
+        CO: 0.4,
+        SO2: 0.002,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 39,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동북권",
+        MSRSTE_NM: "노원구",
+        PM10: 21,
+        PM25: 14,
+        O3: 0.017,
+        NO2: 0.016,
+        CO: 0.4,
+        SO2: 0.004,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 36,
+        ARPLT_MAIN: "PM25"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서남권",
+        MSRSTE_NM: "강서구",
+        PM10: 36,
+        PM25: 16,
+        O3: 0.021,
+        NO2: 0.013,
+        CO: 0.4,
+        SO2: 0.004,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 42,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서남권",
+        MSRSTE_NM: "구로구",
+        PM10: 23,
+        PM25: 10,
+        O3: 0.022,
+        NO2: 0.016,
+        CO: 0.3,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 37,
+        ARPLT_MAIN: "O3"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서남권",
+        MSRSTE_NM: "영등포구",
+        PM10: 29,
+        PM25: 15,
+        O3: 0.01,
+        NO2: 0.022,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 41,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서남권",
+        MSRSTE_NM: "동작구",
+        PM10: 29,
+        PM25: 15,
+        O3: 0.012,
+        NO2: 0.023,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 41,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서남권",
+        MSRSTE_NM: "관악구",
+        PM10: 27,
+        PM25: 12,
+        O3: 0.012,
+        NO2: 0.022,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 37,
+        ARPLT_MAIN: "NO2"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서남권",
+        MSRSTE_NM: "금천구",
+        PM10: 25,
+        PM25: 15,
+        O3: 0.015,
+        NO2: 0.02,
+        CO: 0.4,
+        SO2: 0.004,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 43,
+        ARPLT_MAIN: "PM25"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "서남권",
+        MSRSTE_NM: "양천구",
+        PM10: 0,
+        PM25: 14,
+        O3: 0.016,
+        NO2: 0.017,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "점검중",
+        IDEX_MVL: -99,
+        ARPLT_MAIN: "점검중"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동남권",
+        MSRSTE_NM: "강남구",
+        PM10: 31,
+        PM25: 16,
+        O3: 0.018,
+        NO2: 0.018,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 39,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동남권",
+        MSRSTE_NM: "서초구",
+        PM10: 34,
+        PM25: 13,
+        O3: 0.024,
+        NO2: 0.019,
+        CO: 0.3,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 41,
+        ARPLT_MAIN: "PM10"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동남권",
+        MSRSTE_NM: "송파구",
+        PM10: 25,
+        PM25: 6,
+        O3: 0.014,
+        NO2: 0.025,
+        CO: 0.4,
+        SO2: 0.003,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 42,
+        ARPLT_MAIN: "NO2"
+    },
+    {
+        MSRDT: "201912052100",
+        MSRRGN_NM: "동남권",
+        MSRSTE_NM: "강동구",
+        PM10: 24,
+        PM25: 14,
+        O3: 0.016,
+        NO2: 0.02,
+        CO: 0.4,
+        SO2: 0.002,
+        IDEX_NM: "좋음",
+        IDEX_MVL: 39,
+        ARPLT_MAIN: "PM25"
+    }
+]
+
+mise.forEach(element => {
+    if (element["IDEX_MVL"] < 50) {
+        console.log(element["MSRSTE_NM"], element["IDEX_MVL"])
+    }
+})
+
+/*
+    5.
+    FizzBuzz 문제
+
+    3의 배수면 Fizz, 5의 배수면 Buzz, 15의 배수면 FizzBuzz
+*/
+
+
+function getFizzBuzz(n) {
+    if (n % 3 == 0 && n % 5 == 0) {
+        return "FizzBuzz"
+    } else if (n%3 == 0) {
+        return "Fizz"
+    } else {
+        return "Buzz"
+    }
+}
+
