ab0c8e2a530b1604bf1a2d6cafbe6f7e6b64c1fe
jinnify <seungjin429@gmail.com>
2020-04-06 20:06:58 +0900

    add pymongo

--- 9Day/app.py
+++ 9Day/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, jsonify, request
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import requests
+
+
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-
-from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
